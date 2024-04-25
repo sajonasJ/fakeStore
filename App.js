6a026d9aa76e7cd210499f95bc6254ccf8b7eb2a
sajonasJ <jonas.sajonas@gmail.com>
2024-04-25 11:37:00 +1000

    added splash screen and buttons

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Splash from "./src/screens/Splash";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/Splash.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions } from "react-native";
+import { ImageBackground } from "react-native";
+
+const windowHeight = Dimensions.get('window').height;
+const splashImage = require("../public/splash.png");
+
+export default function Splash() {
+    const handlePress = () => {
+        // Handle button press
+      };
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <ImageBackground
+        source={splashImage}
+        style={styles.container}
+      >
+        <View style={styles.innerContainer}>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text style={styles.buttonText}>Enter</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      innerContainer: {
+        width: "50%",
+        marginTop: windowHeight * 0.5,
+      },
+      button: {
+        backgroundColor: "maroon",
+        padding: 10,
+        borderRadius: 35,
+      },
+      buttonText: {
+        color: "#fff",
+        textAlign: "center",
+        fontWeight:"bold",
+        fontSize:18,
+      },
+    });
